80e49069db68d040bb6ef8a3a1865f60798b6ce6
finnmmmmm <101579869+finnmmmmm@users.noreply.github.com>
2022-05-26 13:46:11 -0500

    testing applets

---
+++ HelloWorldApplet.java
@@ -0,0 +1,9 @@
+import java.applet.*;
+import java.awt.*;
+
+public class FoodTrackerApplet extends Applets {
+    
+    public void paint(Graphics g);
+    g.drawString("Hello World". 25, 50);
+
+}
